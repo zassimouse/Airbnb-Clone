b38d1b96c9788bfe256b7578534189dc355d860d
Denis Haritonenko <114603705+zassimouse@users.noreply.github.com>
2024-07-30 16:58:20 +0300

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Denis Haritonenko on 30.07.24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
