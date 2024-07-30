bb99540e6434fc16ba84454092704052fc91a8f2
Denis Haritonenko <114603705+zassimouse@users.noreply.github.com>
2024-07-30 19:06:30 +0300

    ADD Search Bar

---
+++ AirbnbClone/Core/Explore/SearchAndFilterBar.swift
@@ -0,0 +1,46 @@
+//
+//  SearchAndFilterBar.swift
+//  AirbnbClone
+//
+//  Created by Denis Haritonenko on 30.07.24.
+//
+
+import SwiftUI
+
+struct SearchAndFilterBar: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+            
+            VStack(alignment: .leading, spacing: 2) {
+                Text("Where to?")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                
+                Text("Anywhere - Any Week - Add guests")
+                    .font(.caption2)
+                    .foregroundStyle(.gray)
+            }
+            
+            Spacer()
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image(systemName: "line.3.horizontal.decrease.circle")
+                    .foregroundStyle(.black)
+            })
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 10)
+        .overlay {
+            Capsule()
+                .stroke(lineWidth: 0.5)
+                .foregroundStyle(Color(.systemGray4))
+                .shadow(color: .black.opacity(0.4), radius: 2)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    SearchAndFilterBar()
+}
